11dee82871dcf21c7e6b968ec3c575924b0ba733
99003716 <mithun.mr@ltts.com>
2021-02-27 21:26:42 +0530

    layout for email

--- 3_Implementation/pr_email.py
+++ 3_Implementation/pr_email.py
@@ -6,3 +6,19 @@ from email.message import EmailMessage
 ADDRESS = os.environ.get('EMAIL_USER')
 PASSWORD = os.environ.get('EMAIL_PASS')
 
+msg = EmailMessage()
+msg['Subject']
+msg['From']
+msg['To']
+msg.set_content()
+
+with open('random.jpg', 'r') as f:
+    file_data = f.read()
+    file_type = imghdr.what(f.name)
+    file_name = f.name
+    
+msg.add_attachment(file_data, maintype='image', subtype=file_type, filename=file_name)
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+    smtp.login(ADDRESS, PASSWORD)
+    smtp.send_message(msg)
