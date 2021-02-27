68594d766d170b84cfaac9b7fc3a5b254f117c71
99003650 <78997874+99003650@users.noreply.github.com>
2021-02-27 23:18:14 +0530

    Add files via upload

---
+++ 3_Implementation/sdlc project.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import matplotlib
+data=pd.read_excel("presur.xlsx",header=1)
+print(data)
+#sum=data['LO1'].sum()
+#no_of_rows=data.shape[0]
+#average=sum/no_of_rows
+#print(average)
+def min_of_LO():
+    for i in range(1,7):
+        a="LO"+str(i)
+        min_lo=data[a].min()
+        print(min_lo)
+min_of_LO()
+def max_of_LO():
+    for i in range(1,7):
+        a="LO"+str(i)
+        max_lo=data[a].max()
+        print(max_lo)
+max_of_LO()
+
+def total_marks_of_student():
+    for i in range(0,4):
+        av=data.iloc[i,2:].sum()
+        print(av)   
+total_marks_of_student()
