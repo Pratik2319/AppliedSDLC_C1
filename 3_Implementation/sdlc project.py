fdf2d7c40015c61f7175149e05eff9c2aa0fcc78
99003716 <mithun.mr@ltts.com>
2021-02-28 10:59:48 +0530

    using os module for saving files

--- 3_Implementation/sdlc project.py
+++ 3_Implementation/sdlc project.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import os
 from matplotlib import pyplot as plt
 data=pd.read_excel("presur.xlsx",header=1)
 print(data)
@@ -39,14 +40,23 @@ total_marks_of_student()
 
 x_lst_all = ["LO1", "LO2", "LO3", "LO4", "LO5", "LO6"]
 
-def auto_co_plotting(y_lst, x_lst):
+def auto_co_plotting(y_lst, x_lst, y):
     plt.bar(x_lst, y_lst, color="#000000", label="Student's Performance")
+    plt.xlabel('Learning Objectives')
+    plt.ylabel('Test/Survey points')
+    path = f"C:/Users/mithu/Desktop/demo/{y}"
+    os.mkdir(path)
+    plt.savefig(f"{y}/plot.png")
+
 
 def cross_co_plotting(y_lst2, y_lst1, x_lst):
     x_ind = np.arange(len(x_lst))
     width = 0.3
     plt.bar(x_ind, y_lst2, color="#000000", label="Pre-test", width=0.3)
     plt.bar(x_ind + width, y_lst1, color="#ff00ff", label="Post-test", width=0.3)
+    plt.xlabel('Learning Objectives')
+    plt.ylabel('Test/Survey points')
+    plt.savefig()
 
 
 
